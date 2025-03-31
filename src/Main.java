e30d7ad420e367ba99573136d2f6799b19ddb982
Alina Abdullaeva <areon.aur@mail.ru>
2025-03-31 18:51:27 +0300

    Added 3 euro

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("The empty wallet yet");
         System.out.println("Added 1 euro");
+        System.out.println("Added 3 euro");
     }
 }
