09a93c1b6810ed2a1a00d631a42943ef88d51ce8
Alina Abdullaeva <areon.aur@mail.ru>
2025-03-31 19:21:06 +0300

    Added pretty

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("The empty wallet yet");
+
         System.out.println("Added 1 rub");
         System.out.println("Added 7 rub");
+
         System.out.println("Added 1 euro");
         System.out.println("Added 3 euro");
         System.out.println("Added 4 euro");
