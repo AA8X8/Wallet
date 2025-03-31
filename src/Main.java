96469fb31c69a216fa730cc434cfab99b6fd5e83
Alina Abdullaeva <areon.aur@mail.ru>
2025-03-31 18:50:38 +0300

    Added 7 rub

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("The empty wallet yet");
         System.out.println("Added 1 rub");
+        System.out.println("Added 7 rub");
     }
 }
